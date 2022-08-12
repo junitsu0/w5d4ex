1ed070d0e9caab97b1a525e506db966d6a87c055
Justin <junitsu00@gmail.com>
2022-08-12 10:19:26 -0500

    saved

--- conjuction function.sql
+++ conjuction function.sql
@@ -1,39 +1,40 @@
 --Question 1
 
 create procedure newfilm
+	(VARCHAR(255), TEXT, integer, INTeger, INTeger, NUMERIC(4,2), INTeger, numeric(4,2), mpaa_rating)
 language plpgsql
 as $$
 begin
-	insert into film(
-	title, description, release_year, language_id, rental_duration,
+	insert into film(title, description, release_year, language_id, rental_duration,
 	rental_rate, length, replacement_cost, rating)
-		values (VARCHAR(255), TEXT, YEAR, INT2, INT2, 
-		NUMERIC(4,2), INT2, numeric(4,2), mpaa_rating);
+		values (title, description, release_year, language_id, rental_duration,
+	rental_rate, length, replacement_cost, rating);
 end
 $$
 
 call newfilm('Ichi the Killer', 'As sadomasochistic yakuza enforcer Kakihara searches
 	for his missing boss he comes across Ichi, a repressed and psychotic killer who
 	may be able to inflict levels of pain that Kakihara has only dreamed of achieving.', 
-	2001, 1, 3, 3.99, 129, 39.99, 'Unrated')
+	2001, 1, 3, 3.99, 129, 39.99, 'R')
 
 ------------------------------------------------------------------------------------
 --Question 2
 
-create or replace function get_category_count(category_id integer)
+create or replace function get_category_count(cat_id integer)
 returns integer
 language plpgsql
 as $$
 	declare category_count integer;
 begin
-	select count(category_id) into category_count
+	select count(*) into category_count
 	from film_category
+	where category_id = cat_id
 	group by category_id;
 	return category_count;
 end;
 $$;
 
-select get_category_count(7);
+select get_category_count(5);
 
 
 ------------------------------------------------------------------------------------
